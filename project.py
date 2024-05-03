5b5f0756b23a02d103e813d1c2fada1e242e5011
Michael Rutkoske <budrutkoske@yahoo.com>
2024-05-03 13:13:52 -0500

    modified game loop and room options, win scenarios

--- project.py
+++ project.py
@@ -84,7 +84,6 @@ def choose_room(choices):
     choice = input("Enter the number of your choice: ")
     return choices[int(choice) - 1]
 
-
 # Function to play the game
 def play_game():
     print("Welcome to Anna's Adventure!")
